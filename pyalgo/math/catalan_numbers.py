21a3562f1961ad640db348a46ba026daabfd06d4
Devansh3712 <devanshamity@gmail.com>
2021-02-02 17:43:06 +0530

    add catalan_numbers to math

---
+++ pyalgo/math/catalan_numbers.py
@@ -0,0 +1,35 @@
+'''
+module for calculating
+the nth catalan number
+'''
+
+def binomial_coeff(n: int, k: int):
+
+    if (k > n - k):
+        k = n - k
+
+    result = 1
+
+    for i in range (k):
+        result *= (n - i)
+        result /= (i + 1)
+
+    return result
+
+def catalan(n: int):
+
+    '''
+    Using binomial coefficients
+    calculating the nth
+    catalan number
+    '''
+
+    result = binomial_coeff(2 * n, n)
+    answer = result / (n + 1)
+
+    return answer
+
+'''
+PyAlgo
+Devansh Singh, 2021
+'''
