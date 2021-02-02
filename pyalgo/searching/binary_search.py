53a2f0cb11c6a100ab79e555b0cd34e19510f05a
Devansh3712 <devanshamity@gmail.com>
2021-02-02 13:33:47 +0530

    update binary_search

---
+++ pyalgo/searching/binary_search.py
@@ -0,0 +1,38 @@
+'''
+module for implementation
+of binary search
+algorithm
+'''
+
+def binary_search(arr: list, start: None, end: None, num):
+
+    '''
+    Binary search implementation
+    using iterative method
+    '''
+
+    if start == None:
+        start = 0
+
+    if end == None:
+        end = len(arr)
+
+    while (start <= end):
+
+        mid = start + ((end - start) // 2)
+
+        if (arr[mid] < num):
+            start = mid + 1
+
+        elif arr[mid] > num:
+            end = mid - 1
+
+        else:
+            return mid
+
+    return -1
+
+'''
+PyAlgo
+Neemesh Yadav, 2021
+'''
