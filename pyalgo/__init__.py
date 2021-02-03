d60656c88f61daa0b620ad2b675716d7c3dd31f3
Devansh3712 <devanshamity@gmail.com>
2021-02-03 20:24:47 +0530

    update library

---
+++ pyalgo/__init__.py
@@ -0,0 +1,32 @@
+'''
+module for importing
+all functions
+'''
+
+'''
+PyAlgo - Maths
+'''
+
+from .math.catalan_numbers import catalan
+from .math.factorial import factorial
+from .math.fibonnaci_numbers import fibonacci
+from .math.gcd import gcd, lcm
+from .math.power import mod_power, big_power
+from .math.prime import prime
+from .math.sieve import sieve
+from .math.totient_function import totient
+
+'''
+PyAlgo - Searching
+'''
+
+from .search.binary_search import binary_search
+from .search.exponential_search import exponential_search
+from .search.interpolation_search import interpolation_search
+from .search.jump_search import jump_search
+from .search.ternary_search import ternary_search
+
+'''
+PyAlgo
+Devansh Singh, 2021
+'''
