39deef9493cecae870e7dca07793eeefeb5b4f5b
Devansh3712 <devanshamity@gmail.com>
2021-02-11 20:17:22 +0530

    update setup.py

--- pyalgo/__init__.py
+++ pyalgo/__init__.py
@@ -3,42 +3,61 @@ module for importing
 all functions
 '''
 
+
+import os, sys
+
+sys.path.insert(0, os.path.abspath('.'))
+
+from pyalgo import *
+
 '''
 PyAlgo - Maths
 '''
 
-from .maths import *
-from .maths.catalan_numbers import catalan
-from .maths.factorial import factorial
-from .maths.fibonnaci_numbers import fibonacci
-from .maths.gcd import gcd, lcm
-from .maths.power import mod_power, big_power
-from .maths.prime import prime
-from .maths.sieve import sieve
-from .maths.totient_function import totient
-from .maths.gray_code import gray_code
+from pyalgo.maths import *
+from pyalgo.maths.catalan_numbers import catalan
+from pyalgo.maths.factorial import factorial
+from pyalgo.maths.fibonnaci_numbers import fibonacci
+from pyalgo.maths.gcd import gcd, lcm
+from pyalgo.maths.power import mod_power, big_power
+from pyalgo.maths.prime import prime
+from pyalgo.maths.sieve import sieve
+from pyalgo.maths.totient_function import totient
+from pyalgo.maths.gray_code import gray_code
 
 '''
 PyAlgo - Searching
 '''
 
-from .search import *
-from .search.binary_search import binary_search
-from .search.exponential_search import exponential_search
-from .search.interpolation_search import interpolation_search
-from .search.jump_search import jump_search
-from .search.ternary_search import ternary_search
-from .search.kmp_algorithm import kmp
-from .search.rabin_karp import rabin_karp
-from .search.z_algorithm import z_algorithm
+from pyalgo.search import *
+from pyalgo.search.binary_search import binary_search
+from pyalgo.search.exponential_search import exponential_search
+from pyalgo.search.interpolation_search import interpolation_search
+from pyalgo.search.jump_search import jump_search
+from pyalgo.search.ternary_search import ternary_search
+from pyalgo.search.kmp_algorithm import kmp
+from pyalgo.search.rabin_karp import rabin_karp
+from pyalgo.search.z_algorithm import z_algorithm
 
 '''
 PyAlgo - CP
 '''
 
-from .cp import *
-from .cp.FastIO import *
-from .cp.template import *
+from pyalgo.cp import *
+from pyalgo.cp.FastIO import *
+from pyalgo.cp.template import *
+
+'''
+PyAlgo - Sort
+'''
+
+from pyalgo.sort import *
+from pyalgo.sort.bubble_sort import bubble_sort
+from pyalgo.sort.heap_sort import heap_sort
+from pyalgo.sort.insertion_sort import insertion_sort
+from pyalgo.sort.merge_sort import merge_sort
+from pyalgo.sort.quick_sort import quick_sort, quick_sort_recursive
+from pyalgo.sort.selection_sort import selection_sort
 
 '''
 PyAlgo
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(directory, 'README.md'), encoding = 'utf-8') as f:
 
 setup( 
         name ='pyalgo-lib',
-        version = '0.1',
+        version = '0.1.1',
         author ='Devansh Singh',
         author_email ='devanshamity@gmail.com',
         url ='https://github.com/Devansh3712/PyAlgo',
