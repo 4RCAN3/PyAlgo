c14ffd56e4e10aadfa963d89a0ce0745c050cc61
Devansh <58616444+Devansh3712@users.noreply.github.com>
2021-02-11 18:09:28 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from os import path
 
 directory = path.abspath(path.dirname(__file__))
 
-with open(path.join(this_directory, 'README.md'), encoding = 'utf-8') as f:
+with open(path.join(directory, 'README.md'), encoding = 'utf-8') as f:
     long_description = f.read()
 
 setup( 
